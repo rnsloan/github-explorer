fc3b66185bddc7ffeb84ca638ef1d9f90e56f5dd
rnsloan <rnsloan1@gmail.com>
2018-02-19 18:30:17 +1100

    add homepage trending repos

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,21 @@
 import React from "react";
 import { gql } from "apollo-boost";
 import { Query } from "react-apollo";
+import { DateTime } from "luxon";
 import Header from "../compoonents/Header/Header";
+import Spinner from "../compoonents/Spinner";
+import "./Home.css";
 
-const GET_DOG = gql`
+const dt = DateTime.local()
+  .minus({ days: 7 })
+  .toISODate();
+const query = gql`
   query {
-    search(query: "lang:javascript", type: REPOSITORY, first: 20) {
+    search(
+      query: "stars:>300 created:>${dt}"
+      type: REPOSITORY
+      first: 10
+    ) {
       edges {
         node {
           ...Repo
@@ -15,37 +25,98 @@ const GET_DOG = gql`
   }
 
   fragment Repo on Repository {
-    name
+    nameWithOwner
     shortDescriptionHTML
+    owner {
+      avatarUrl
+    }
+    stargazers {
+      totalCount
+    }
   }
 `;
 
-const Home = () => (
-  <Query query={GET_DOG}>
-    {({ loading, error, data }) => {
-      console.log(loading, error, data);
-      return (
-        <div>
-          <Header />
-          <div className="uk-text-center uk-card uk-card-default uk-padding-small">
-            <form
-              className="uk-search uk-search-default"
-              style={{ width: "80%" }}
-            >
-              <input
-                className="uk-search-input"
-                type="search"
-                placeholder="Search GitHub"
-              />
-            </form>
-            <h1 className="uk-heading-line">
-              <span>Trending repositories</span>
-            </h1>
-          </div>
+const Repo = ({ data }) => {
+  return (
+    <article className="uk-card uk-card-small uk-card-body uk-card-default">
+      <header className="uk-grid uk-grid-small uk-flex-middle">
+        <div className="uk-width-auto">
+          <img
+            src={data.owner.avatarUrl}
+            alt={`${data.owner.login} avatar`}
+            width="50"
+            height="50"
+          />
         </div>
-      );
-    }}
-  </Query>
+        <div className="uk-width-expand">
+          <h4>
+            <a href="#">{data.nameWithOwner}</a>&nbsp;
+            <span
+              title="stars in the last week"
+              aria-label="stars in the last week"
+              className="uk-badge"
+            >
+              {data.stargazers.totalCount}
+            </span>
+          </h4>
+        </div>
+      </header>
+      <div className="uk-padding-small uk-padding-remove-horizontal uk-padding-remove-bottom">
+        <p
+          dangerouslySetInnerHTML={(() => {
+            return { __html: `${data.shortDescriptionHTML}` };
+          })()}
+        />
+      </div>
+    </article>
+  );
+};
+
+const Home = () => (
+  <div>
+    <Header />
+    <div className="uk-card uk-card-default uk-padding-small">
+      <div className="uk-text-center">
+        <form className="uk-search uk-search-default width80">
+          <input
+            className="uk-search-input"
+            type="search"
+            placeholder="Search GitHub"
+          />
+        </form>
+      </div>
+      <Query query={query}>
+        {({ loading, error, data }) => {
+          let html = (
+            <div className="uk-text-center">
+              <Spinner />
+            </div>
+          );
+
+          if (data) {
+            html = (
+              <div className="repo-grid">
+                {data.search.edges.map(data => {
+                  return (
+                    <Repo key={data.node.nameWithOwner} data={data.node} />
+                  );
+                })}
+              </div>
+            );
+          }
+
+          return (
+            <div>
+              <h1 className="uk-text-center uk-heading-line">
+                <span>Popular repositories</span>
+              </h1>
+              {html}
+            </div>
+          );
+        }}
+      </Query>
+    </div>
+  </div>
 );
 
 export default Home;
